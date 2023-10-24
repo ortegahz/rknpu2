ed08b893d8300cbc69fc1ab4cd2bd1dd163edede
manu <zxthz@126.com>
2023-10-24 10:35:34 +0800

    format

--- examples/rknn_yolov5_demo/include/postprocess.h
+++ examples/rknn_yolov5_demo/include/postprocess.h
@@ -15,10 +15,10 @@
 
 #define OBJ_NAME_MAX_SIZE 16
 #define OBJ_NUMB_MAX_SIZE 64
-#define OBJ_CLASS_NUM     1
-#define NMS_THRESH        0.45
-#define BOX_THRESH        0.5
-#define PROP_BOX_SIZE     (5+OBJ_CLASS_NUM)
+#define OBJ_CLASS_NUM 1
+#define NMS_THRESH 0.45
+#define BOX_THRESH 0.5
+#define PROP_BOX_SIZE (5 + OBJ_CLASS_NUM)
 
 #define KPS_PIXEL_STD 200
 #define KPS_PIXEL_BORDER 10
@@ -26,7 +26,7 @@
 #define KPS_KEYPOINT_NUM 17
 #define KPS_INPUT_SHAPE_H 256
 #define KPS_INPUT_SHAPE_W 192
-#define KPS_WIDTH_HEIGHT_RATIO ((float) KPS_INPUT_SHAPE_W / (float) KPS_INPUT_SHAPE_H)
+#define KPS_WIDTH_HEIGHT_RATIO ((float)KPS_INPUT_SHAPE_W / (float)KPS_INPUT_SHAPE_H)
 #define KPS_OUTPUT_SHAPE_H 64
 #define KPS_OUTPUT_SHAPE_W 48
 #define KPS_STRIDE 4
@@ -118,17 +118,17 @@ typedef struct _kps_result_group_t
     kps_result_t results[OBJ_NUMB_MAX_SIZE];
 } kps_result_group_t;
 
-template<typename t>
-int post_process_player_6(t* input0, t* input1, t* input2, t* input3, t* input4, int model_in_h, int model_in_w, float conf_threshold, float nms_threshold, float scale_w, float scale_h, std::vector<int32_t>& qnt_zps, std::vector<float>& qnt_scales, detect_result_group_float_t* group);
+template <typename t>
+int post_process_player_6(t *input0, t *input1, t *input2, t *input3, t *input4, int model_in_h, int model_in_w, float conf_threshold, float nms_threshold, float scale_w, float scale_h, std::vector<int32_t> &qnt_zps, std::vector<float> &qnt_scales, detect_result_group_float_t *group);
 
-template<typename t>
+template <typename t>
 int post_process_kps(t *pt8Input, std::vector<uint32_t> &qnt_zps, std::vector<float> &qnt_scales, float fCenterX, float fCenterY, float fScaleWT, float fScaleHT, kps_result_group_t *group);
 
 int post_process_kps_wrapper(rknn_context ctx_kps, cv::Mat *Img, pcBOX_RECT_FLOAT stBoxRect, void *resize_buf, rknn_tensor_attr *output_attrs, kps_result_group_t *pKps_result_group, bool bF16);
 
-int post_process_acfree(int8_t* input0, int8_t* input1, int8_t* input2, int8_t* input3, int8_t* input4, int8_t* input5, int model_in_h, int model_in_w, float conf_threshold,
-                 float nms_threshold, float scale_w, float scale_h, std::vector<int32_t>& qnt_zps,
-                 std::vector<float>& qnt_scales, detect_result_group_t* group);
+int post_process_acfree(int8_t *input0, int8_t *input1, int8_t *input2, int8_t *input3, int8_t *input4, int8_t *input5, int model_in_h, int model_in_w, float conf_threshold,
+                        float nms_threshold, float scale_w, float scale_h, std::vector<int32_t> &qnt_zps,
+                        std::vector<float> &qnt_scales, detect_result_group_t *group);
 
 int post_process(int8_t *input0, int8_t *input1, int8_t *input2, int model_in_h, int model_in_w,
                  float conf_threshold, float nms_threshold, float scale_w, float scale_h,
